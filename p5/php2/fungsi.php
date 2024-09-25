490bd24895ed937b436f656bb10803e409256ed9
Muhammad Rafi Rajendra <rafi53417@gmail.com>
2024-09-25 19:50:50 +0700

    Memangil Funsi di dalam fungsi

--- p5/php2/fungsi.php
+++ p5/php2/fungsi.php
@@ -4,5 +4,13 @@ function hitungUmur($thn_lahir, $thn_sekarang) {
     return $umur;
 }
 
-echo "Umur saya adalah " . hitungUmur(2004, 2024) . " tahun";
+function perkenalan ($nama, $salam="Assalamualaikum"){
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ".$nama. "<br>";
+
+    echo "Saya berusia ". hitungUmur(2004, 2024) . " tahun <br>";
+    echo "Senang berkenalan dengan anda <br>";
+}
+
+perkenalan("Rafi");
 ?>
