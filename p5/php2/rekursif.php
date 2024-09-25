215c480302d97202157db87fe7501dab62caf85b
Muhammad Rafi Rajendra <rafi53417@gmail.com>
2024-09-25 20:40:58 +0700

    Kasus Menu Bertingkat

--- p5/php2/rekursif.php
+++ p5/php2/rekursif.php
@@ -1,10 +1,48 @@
 <?php
-function tampilkanAngka (int $jumlah, int $indeks = 1){
-    echo "Perulangan ke-{$indeks} <br>";
+$menu = [
+    [
+        "nama" => "Beranda"
+    ],
+    [
+        "nama" => "Berita",
+        "subMenu" => [
+            [
+                "nama" => "Wisata",
+                "subMenu" => [
+                    [
+                        "nama" => "Pantai"
+                    ],
+                    [
+                        "nama" => "Gunung"
+                    ]
+                ]
+            ],
+            [
+                "nama" => "Kuliner"
+            ],
+            [
+                "nama" => "Hiburan"
+            ]
+        ]
+    ],
+    [
+        "nama" => "Tentang"
+    ],
+    [
+        "nama" => "Kontak"
+    ],
+];
 
-    if ($indeks < $jumlah) {
-        tampilkanAngka($jumlah, $indeks + 1);
+function tampilkanMenuBertingkat(array $menu) {
+    echo "<ul>";
+    foreach ($menu as $key => $item) {
+        echo "<li>{$item['nama']}</li>";
+        if (isset($item['subMenu'])) {
+            tampilkanMenuBertingkat($item['subMenu']);
+        }
     }
+    echo "</ul>";
 }
-tampilkanAngka(20);
+
+tampilkanMenuBertingkat($menu);
 ?>
