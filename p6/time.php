ee6065f0df7258747077d16fed684c74ab402a74
Muhammad Rafi Rajendra <rafi53417@gmail.com>
2024-10-03 08:25:38 +0700

    Date & Time

---
+++ p6/time.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Time</title>
+</head>
+<body>
+    <h3>Time</h3>
+    <?php
+        date_default_timezone_set("asia/jakarta");
+        echo date ("h:i:sa");
+    ?>
+</body>
+</html>
